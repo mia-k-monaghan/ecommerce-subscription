0265b6b29ec7fe00f256ea032b2cfc4750792a82
mia-k-monaghan <emmykperdue@gmail.com>
2020-12-15 09:29:33 -0700

    added profile update urls

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignupView, LoginView,ProfileView
+from .views import SignupView, LoginView,ProfileView, ProfileUpdate, ShippingUpdateView
 from django.contrib.auth import views as auth_views
 
 app_name = 'users'
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', SignupView.as_view(), name='index'),
     path('profile/<pk>',ProfileView.as_view(), name='profile'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile-update/<pk>',ProfileUpdate.as_view(), name='profile-update'),
+    path('shipping-update/<pk>',ShippingUpdateView.as_view(), name='shipping-update'),
+
 ]
