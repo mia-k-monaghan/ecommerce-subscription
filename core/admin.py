768cb0cf5172a438ade19e73772cc149b18db629
mia-k-monaghan <emmykperdue@gmail.com>
2020-12-15 15:14:55 -0700

    customized admin to include search filters & list displays

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Subscription, ShippingAddress
 # Register your models here.
 
-admin.site.register(Subscription)
-admin.site.register(ShippingAddress)
+class SubscriptionAdmin(admin.ModelAdmin):
+
+    list_filter = ['active']
+    search_fields = ['user__email', 'shipping_address__state','user__stripe_customer','stripe_subscription']
+    list_display = ['user','shipping_address', 'active']
+    list_display_links = ['user','shipping_address']
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    search_fields = ['user__email', 'city','state','user__stripe_customer','zip','street_address']
+    list_display = ['user','street_address','city','state','zip']
+    list_display_links = ['user']
+
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
